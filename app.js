b792adfc121dcdfa4ed3bf48c04ffb6cc776150e
Anggi Mondera <gie.mondera@gmail.com>
2018-11-17 21:07:01 +0700

    komit dong

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //setup halaman index
 app.get('/', function(req, res){
-    res.send("ini adalah halaman index")     
+    res.send("ini adalah halaman index pertamaku hahahayy") 
 });
 
 
